7c96bfb84a7a1e3ce26288261d152ba4eb618327
Markyute <jmci2023-8651-76533@bicol-u.edu.ph>
2025-03-25 14:53:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
  
-// Define a basic route
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+ 
 app.get('/', (req, res) => {
-    res.send('Hello, Express Server!');
+   
+res.sendFile(path.join(__dirname, 'index.html'));
 });
  
-// Start the server
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server running on http://localhost:3000');
 });
